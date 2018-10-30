434138a632a5ae60610e77aa7fbc87f7f6dd4aaa
Kyle Murphy <kcmurphy82@gmail.com>
2018-10-29 21:01:40 -0500

    Final changes

--- main.py
+++ main.py
@@ -11,7 +11,28 @@ def index():
 
 @app.route('/', methods=['POST'])
 def handleSubmit():
-    return render_template("welcome.html")
+    userid = request.form.get("username")
+    verify = request.form.get("verify")
+    password = request.form.get("password")
+    email = request.form.get("email")
+    error = ""
+    error_count = 0
+    if verify != password:
+        error = "Password must match above."
+        error_count = 1
+        return render_template("index.html", userid=userid, pass_valid=error)
 
+    
+    if email != "":
+        count_amp = email.count("@")
+        count_punto = email.count(".")
+        count_sp = email.count(" ")
+        if not(count_amp == 1 and count_punto == 1 and count_sp == 0):
+            error = "Not a valid email"
+            error_count = 1
+            return render_template("index.html", userid=userid, email_valid=error)
+
+    if error_count < 1:
+        return render_template("welcome.html", userid=userid)
 
 app.run()
